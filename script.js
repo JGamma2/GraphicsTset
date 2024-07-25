7e42db67cfc238379febe4881bdb2326f9552083
Jonathan Hill <jwhill1452@gmail.comm>
2024-07-25 16:34:23 -0500

    New funcitons. These do nothing really. Init will eventually perhaps.

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 canvas.width = window.screen.availWidth-10;
 canvas.height = window.screen.availHeight-100;
+
+//Player data.
+let playerXPosition, playerYPosition;
+function drawPlayer() {   
+    ctx.fillStyle = "#543423"
+    ctx.fillRect(playerXPosition, playerYPosition, 10, 10);
+};
+
+//Game initialization.
+function init() {
+    return 0
+};
+
+
+
+
+
+
+
+
+/*Tester Scripts go in here.
+
+*/
+
+playerXPosition = 100;
+playerYPosition = 100;
+drawPlayer();
