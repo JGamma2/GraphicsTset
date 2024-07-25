233ca6ce308e39ea89a926799395d6353c3351c9
Jonathan Hill <jwhill1452@gmail.comm>
2024-07-25 15:56:19 -0500

    Canvas setup. It's there and should now be ready.

---
+++ script.js
@@ -0,0 +1,5 @@
+//Canvas Setup.
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+canvas.width = window.screen.availWidth-10;
+canvas.height = window.screen.availHeight-100;
