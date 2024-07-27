13cca7d8b84cfa6cede6806d338105c22a8eaa8e
Jonathan Hill <jwhill1452@gmail.comm>
2024-07-27 09:22:34 -0500

    2d fixes. Now the controls are smooth, and the map can be scaled without affecting the important numbers.

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ canvas.width = 1200;
 canvas.height = 900;
 
 //Variable setup.
-let framerate, playerXPosition, playerYPosition, playerAngle, playerSpeed, playerDeltaX, playerDeltaY, currentMap;
+let framerate, playerXPosition, playerYPosition, playerAngle, playerSpeed, playerDeltaX, playerDeltaY, currentMap, map2dScaler;
+let aPressed, dPressed, sPressed, wPressed = false;
 
 //Game map.
 const mapArray1 = [
@@ -27,19 +28,34 @@ const mapArray1 = [
 
 //Key presses.
 function handleKeyPress(k) {
-    //Turns CCW.
     if (k.key == "a") {
-        playerAngle += .1;
-        if (playerAngle >= 2*Math.PI) {
-            playerAngle -= 2*Math.PI;
-        } else if (playerAngle <= 2*Math.PI) {
-            playerAngle -= 2*Math.PI;
-        };
-        playerDeltaX = Math.cos(playerAngle)*playerSpeed;
-        playerDeltaY = Math.sin(playerAngle)*playerSpeed;
-    //Turns CW.
+        aPressed = true;
     } else if (k.key == "d") {
-        playerAngle -= .1;
+        dPressed = true;
+    } else if (k.key == "s") {
+        sPressed = true;
+    } else if (k.key == "w") {
+        wPressed = true;
+    };
+};
+
+function handleKeyUp(k) {
+    
+    if (k.key == "a") {
+        aPressed = false;
+    } else if (k.key == "d") {
+        dPressed = false;
+    } else if (k.key == "s") {
+        sPressed = false;
+    } else if (k.key == "w") {
+        wPressed = false;
+    };
+};
+
+function playerMove() {
+    //Turns CCW.
+    if (aPressed) {
+        playerAngle += .05;
         if (playerAngle >= 2*Math.PI) {
             playerAngle -= 2*Math.PI;
         } else if (playerAngle <= 2*Math.PI) {
@@ -47,14 +63,27 @@ function handleKeyPress(k) {
         };
         playerDeltaX = Math.cos(playerAngle)*playerSpeed;
         playerDeltaY = Math.sin(playerAngle)*playerSpeed;
-    //Moves backward.
-    } else if (k.key == "s") {
-        playerXPosition -= playerDeltaX;
-        playerYPosition += playerDeltaY;
+    };
+    //Turns CW.
+    if (dPressed) {
+        playerAngle -= .05;
+        if (playerAngle >= 2*Math.PI) {
+            playerAngle -= 2*Math.PI;
+        } else if (playerAngle <= 2*Math.PI) {
+            playerAngle -= 2*Math.PI;
+        };
+        playerDeltaX = Math.cos(playerAngle)*playerSpeed;
+        playerDeltaY = Math.sin(playerAngle)*playerSpeed;
+    };
     //Moves forward.
-    } else if (k.key == "w") {
+    if (wPressed) {
         playerXPosition += playerDeltaX;
         playerYPosition -= playerDeltaY;
+    };
+    //Moves backward.
+    if (sPressed) {
+        playerXPosition -= playerDeltaX;
+        playerYPosition += playerDeltaY;
     };
 };
 
@@ -65,20 +94,19 @@ function handleKeyPress(k) {
 function draw2dPlayer() {
     //Draws the player box. 
     ctx.fillStyle = "#54aab3"
-    ctx.fillRect(playerXPosition, playerYPosition, 6, 6);
+    ctx.fillRect(playerXPosition*map2dScaler, playerYPosition*map2dScaler, 6*map2dScaler, 6*map2dScaler);
     //Draws his directional line.
     ctx.beginPath();
     ctx.lineWidth = 3;
     ctx.strokeStyle = "orange";
-    ctx.moveTo(playerXPosition+3, playerYPosition+3);
-    ctx.lineTo(playerXPosition+3 + playerDeltaX*5, playerYPosition+3 - playerDeltaY*5);
+    ctx.moveTo((playerXPosition+3)*map2dScaler, (playerYPosition+3)*map2dScaler);
+    ctx.lineTo((playerXPosition+3 + playerDeltaX*25/playerSpeed)*map2dScaler, (playerYPosition+3 - playerDeltaY*25/playerSpeed)*map2dScaler);
     ctx.stroke();
 };
 
 //Draws map for the 2d side.
 //Assumes this is a square. If it's not, unknown result.
 //Like the genius I am, the first array I pass through this is not a square. Worked perfectly tho.
-//Eventually will be phased out for full 3d, but currently required for visualization.
 function draw2dMap(mapArray) {
     let currentRow = 0;
     let currentBlock = 0;
@@ -86,10 +114,10 @@ function draw2dMap(mapArray) {
         for (let j of i) {
             if (j == 1) {
                 ctx.fillStyle = "#ffffff";
-                ctx.fillRect(25*currentBlock,25*currentRow,25,25);
+                ctx.fillRect(25*currentBlock*map2dScaler,25*currentRow*map2dScaler,25*map2dScaler,25*map2dScaler);
             } else if (j == 0) {
                 ctx.fillStyle = "#000000";
-                ctx.fillRect(25*currentBlock,25*currentRow,25,25);
+                ctx.fillRect(25*currentBlock*map2dScaler,25*currentRow*map2dScaler,25*map2dScaler,25*map2dScaler);
             };
             currentBlock++
         };
@@ -112,17 +140,20 @@ function main() {
 };
 
 //Game initialization.
+//If there's some variable that needs to be smth when everything starts, it's probably going to be here.
 function init() {
     framerate = 60;
     playerXPosition = 75;
     playerYPosition = 75;
     playerAngle = 0; //In radians.
-    playerSpeed = 200/framerate;
-    //These variables are confusing, but these are like the length of the legs of the triangle made by the player's angle.
+    playerSpeed = 50/framerate;
+    //These 2 variables are confusing, but these are like the length of the legs of the right triangle made by the player's angle.
     playerDeltaX = Math.cos(playerAngle)*playerSpeed;
     playerDeltaY = Math.sin(playerAngle)*playerSpeed;
-    currentMap = mapArray1;
+    map2dScaler = .4; //Change this to change the size of the 2d map without breaking everything else.
+    currentMap = mapArray1; //There's only 1 rn but I could add another and then switch between maps/levels.
     document.addEventListener("keydown", handleKeyPress);
+    document.addEventListener("keyup", handleKeyUp);
 };
 
 //2d and 3d renderer. This is what get looped by main, happens every frame.
@@ -135,6 +166,7 @@ function displayFrame() {
     //
     //Draws the 2d portion.
     draw2dMap(currentMap);
+    playerMove()
     draw2dPlayer();
     //draw2dRays();
     
